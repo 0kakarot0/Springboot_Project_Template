6928feba588acafc05c380e6780cf1a8482fd32f
Ahtisham <ilyasahtsham@gmail.com>
2025-03-23 21:46:42 +0500

    added config classes

--- src/main/java/com/example/project_template/config/SecurityConfig.java
+++ src/main/java/com/example/project_template/config/SecurityConfig.java
@@ -1,12 +1,49 @@
 package com.example.project_template.config;
 
+import com.example.project_template.security.JwtFilter;
+import com.example.project_template.utils.role.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
-    @Value("${DB_URL}")
-    private String dbUrl;
+    private final JwtFilter filter;
+    private final AuthenticationProvider authenticationProvider;
 
-    @Value("${JWT_SECRET}")
-    private String jwtSecret;
 
+    @Bean
+    private SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        request ->
+                                request.requestMatchers(
+                                                "/api/v1/user/login",
+                                                "/api/v1/user/register",
+                                                "/api/v1/admin/login",
+                                                "/api/v1/manager/login"
+                                        ).permitAll()
+                                        .requestMatchers("/api/v1/user/**").hasAuthority(Role.USER.name()).anyRequest().authenticated()
+                                        .requestMatchers("/api/v1/admin/**").hasAuthority(Role.ADMIN.name()).anyRequest().authenticated()
+                                        .requestMatchers("/api/v1/manager/**").hasAuthority(Role.MANAGER.name()).anyRequest().authenticated()
+                )
+                .sessionManagement(
+                        session ->
+                                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
 }
--- src/main/java/com/example/project_template/config/UserDetailsServiceConfig.java
+++ src/main/java/com/example/project_template/config/UserDetailsServiceConfig.java
@@ -1,4 +1,46 @@
 package com.example.project_template.config;
 
+import com.example.project_template.exception.NoUserFoundWithEmailProvided;
+import com.example.project_template.user.repository.UserRepository;
+import com.example.project_template.utils.error_messages.ErrorMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
 public class UserDetailsServiceConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userEmail -> userRepository.findUserByEmail(userEmail)
+                .orElseThrow(() -> new NoUserFoundWithEmailProvided(ErrorMessage.RESOURCE_NOT_FOUND.getMessage() + " with email " + userEmail));
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
