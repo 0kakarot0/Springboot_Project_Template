ce44f0e6eea8a58bf73c6dfffea9b18b42eb318c
Ahtisham <ilyasahtsham@gmail.com>
2025-03-23 21:47:35 +0500

    added user service, controller, repo, entity classes

--- src/main/java/com/example/project_template/user/repository/UserRepository.java
+++ src/main/java/com/example/project_template/user/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.project_template.user.repository;
 
-public class UserRepository {
+import com.example.project_template.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
 }
--- src/main/java/com/example/project_template/user/service/UserService.java
+++ src/main/java/com/example/project_template/user/service/UserService.java
@@ -1,4 +1,9 @@
 package com.example.project_template.user.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
 }
