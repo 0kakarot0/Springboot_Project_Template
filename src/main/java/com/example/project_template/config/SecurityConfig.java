e41fc4ea29dd14d7d848bbfe965967fe2ade2922
Ahtisham <ilyasahtsham@gmail.com>
2025-03-23 20:13:58 +0500

    fetched the db url and secret in the config from the application file

--- src/main/java/com/example/project_template/config/SecurityConfig.java
+++ src/main/java/com/example/project_template/config/SecurityConfig.java
@@ -1,4 +1,12 @@
 package com.example.project_template.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SecurityConfig {
+    @Value("${DB_URL}")
+    private String dbUrl;
+
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
 }
