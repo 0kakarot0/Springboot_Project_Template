8a729b7bb9d56358242310cfe7ad011f37a7d4d9
Ahtisham <ilyasahtsham@gmail.com>
2025-03-23 21:47:12 +0500

    added role and status enum

--- src/main/java/com/example/project_template/utils/role/Role.java
+++ src/main/java/com/example/project_template/utils/role/Role.java
@@ -1,4 +1,7 @@
 package com.example.project_template.utils.role;
 
 public enum Role {
+    USER,
+    ADMIN,
+    MANAGER
 }
--- src/main/java/com/example/project_template/utils/status/Status.java
+++ src/main/java/com/example/project_template/utils/status/Status.java
@@ -1,4 +1,7 @@
 package com.example.project_template.utils.status;
 
 public enum Status {
+    PENDING,
+    APPROVED,
+    REJECTED
 }
