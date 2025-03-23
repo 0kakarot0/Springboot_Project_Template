ed97930131997aad1dfd1b824d3eb09c2ac379a2
Ahtisham <ilyasahtsham@gmail.com>
2025-03-23 21:46:59 +0500

    added generic dto api response

--- src/main/java/com/example/project_template/utils/generic_response/ApiResponse.java
+++ src/main/java/com/example/project_template/utils/generic_response/ApiResponse.java
@@ -1,4 +1,18 @@
 package com.example.project_template.utils.generic_response;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private LocalDateTime timeStamp;
+    private String message;
+    private T data;
 }
