88a88edfccb99c457033f6ff842b75aed07c6ae8
Ahtisham <ilyasahtsham@gmail.com>
2025-03-23 21:46:00 +0500

    added error messages according to the exceptions

--- src/main/java/com/example/project_template/utils/error_messages/ErrorMessage.java
+++ src/main/java/com/example/project_template/utils/error_messages/ErrorMessage.java
@@ -1,4 +1,19 @@
 package com.example.project_template.utils.error_messages;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ErrorMessage {
+    INVALID_CREDENTIALS("Invalid email or password"),
+    AUTHENTICATION_FAILED("Authentication failed"),
+    AUTHORIZATION_FAILED("You do not have permission to access this resource."),
+    STUDENT_NOT_FOUND("Student not found"),
+    RESOURCE_NOT_FOUND("Requested resource not found"),
+    GENERIC_ERROR_MESSAGE("An unexpected error occurred: "),
+    VALIDATION_ERROR("Invalid request parameters");
+
+
+    private final String message;
 }
